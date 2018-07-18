f2e5400a202d042f44cbfe88f05954d3c5802d75
Suvo <subhabrata.ganguli@honeywell.com>
2018-07-18 15:47:54 -0400

    added project Numbers

---
+++ expts/Hello-World/helloWorld.cpp
@@ -0,0 +1,7 @@
+#include "helloWorld.h"
+
+int main()
+{
+	cout << "Hello World!";
+	return 0;
+}
---
+++ expts/Hello-World/helloWorld.h
@@ -0,0 +1,8 @@
+#ifndef HELLOWORLD_H_INCLUDED
+#define HELLOWORLD_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+
+#endif // HELLOWORLD_H_INCLUDED
